d7e9cb9f15706d5192b46ae4c7f6c007c4c52f4c
Shalap <shalappandotra21@email.com>
2025-01-12 22:19:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>QR Code Generator</h1>
+      <input type="text" 
+      placeholder="e.g: https://google.com" />
+      <button>Generate</button>
+    </div>
+  );
+};
+
+export default App;
